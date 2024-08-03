fb0a2f2be0d6aa115d1a5c4b16862a39669afb43
mmhtoo <mmhtoo.dev@gmail.com>
2024-08-03 09:23:21 +0630

    config: commit-msg linter

---
+++ commitlint.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'ci',
+        'feat',
+        'fix',
+        'refactor',
+        'config',
+        'setup',
+        'style',
+        'docs',
+        'chore',
+        'revert',
+      ],
+    ],
+  },
+}
