05a475f3f9f93640ec8c8b4419cead7917a5846a
mmhkbz <myomin.htoo@kbzbank.com>
2024-08-07 23:52:44 +0630

    wip: task card view section integration

--- src/app/workspace/tasks/(components)/TasksCardViewSection.tsx
+++ src/app/workspace/tasks/(components)/TasksCardViewSection.tsx
@@ -1,5 +1,7 @@
 import {Separator} from '@/components/ui/separator'
 import TaskCard from './TaskCard'
+import {useMemo, useRef} from 'react'
+import {isSameDay, isToday} from 'date-fns'
 
 type TasksCardViewSectionProps = {
   tasks: TaskEntity[]
@@ -8,68 +10,43 @@ type TasksCardViewSectionProps = {
 
 export default function TasksCardViewSection(props: TasksCardViewSectionProps) {
   const {tasks, isPending} = props
+  const currentPivotedDateRef = useRef<Date | null>(null)
+
+  // plain tasks list into same day groupped tasks
+  // const separatedTasks = useMemo(() => {
+  //   const result: TaskEntity[][] = [[]]
+  //   tasks.forEach((task) => {
+  //     // if null, will set first met date to pivoted date
+  //     if (!currentPivotedDateRef.current) {
+  //       currentPivotedDateRef.current = task.task_start
+  //     }
+  //     const hasSameDay = isSameDay(
+  //       currentPivotedDateRef.current,
+  //       task.task_start
+  //     )
+  //     if(hasSameDay){
+  //       const prevTasksForSameDay =
+  //     }
+  //   })
+  // }, [tasks])
+
   return (
     <div className="py-5 flex flex-col gap-5">
+      {/* {tasks.map(task => {
+        if(!currentPivotedDateRef.current){
+          currentPivotedDateRef.current = task.task_start
+        }
+        const isSameDay = isSameDay(currentPivotedDateRef.current,task.task_start)
+        const isToday = isToday(task.task_start)
+        if(isSameDay){
+          return ({
+
+          })
+        }
+      })} */}
       <div>
         <h6 className="font-bold mb-2 text-[14px]">| Today Tasks</h6>
         <div className="grid grid-cols-3 gap-5 md:gap-3">
-          <TaskCard
-            id={crypto.randomUUID()}
-            name="Fix some issues on some portal and release new versions and run test cases"
-            timestamp={new Date()}
-            timeTo={new Date()}
-            timeFrom={new Date()}
-            status="completed"
-            username="Lionel"
-            userId="id"
-          />
-          <TaskCard
-            id={crypto.randomUUID()}
-            name="Fix some issues on some portal and release new versions and run test cases"
-            timestamp={new Date()}
-            timeTo={new Date()}
-            timeFrom={new Date()}
-            status="in-progress"
-            username="Lionel"
-            userId="id"
-          />
-          <TaskCard
-            id={crypto.randomUUID()}
-            name="Fix some issues on some portal and release new versions and run test cases"
-            timestamp={new Date()}
-            timeTo={new Date()}
-            timeFrom={new Date()}
-            status="planned"
-            username="Lionel"
-            userId="id"
-          />
-
-          <TaskCard
-            id={crypto.randomUUID()}
-            name="Fix some issues on some portal and release new versions and run test cases"
-            timestamp={new Date()}
-            timeTo={new Date()}
-            timeFrom={new Date()}
-            status="pending"
-            username="Lionel"
-            userId="id"
-          />
-        </div>
-      </div>
-      <div>
-        <h6 className="font-bold mb-2 text-[14px]">| 2 Aug, 2024 Tasks</h6>
-        <div className="grid grid-cols-3 gap-5 md:gap-3">
-          <TaskCard
-            id={crypto.randomUUID()}
-            name="Fix some issues on some portal and release new versions and run test cases"
-            timestamp={new Date()}
-            timeTo={new Date()}
-            timeFrom={new Date()}
-            status="planned"
-            username="Lionel"
-            userId="id"
-          />
-
           <TaskCard
             id={crypto.randomUUID()}
             name="Fix some issues on some portal and release new versions and run test cases"
